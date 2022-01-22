9141fa68e03dc1787360ab4063094680caf4f4c8
Keith Valin <keith@mailtux.net>
2022-01-22 14:54:36 -0500

    Added create_account function to plugin

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,10 +1,15 @@
 extern crate libloading;
+extern crate polychat_plugin;
 
 use libloading::{Library, Error};
+use polychat_plugin::Account;
+
+type CreateAccountFunc = unsafe fn() -> Account;
 
 pub struct Plugin {
     name: String,
-    lib: Library
+    lib: Library,
+    accounts: Vec<Account>
 }   
 
 impl Plugin {
@@ -21,7 +26,20 @@ impl Plugin {
 
         Ok(Plugin {
             name: name.to_string(),
-            lib: lib.unwrap()
+            lib: lib.unwrap(),
+            accounts: Vec::<Account>::new()
         })
+    }
+
+    pub fn create_account(&self) -> Result<Account, Error> {
+        unsafe {
+            let func = self.lib.get(b"create_account");
+
+            if func.is_err() {
+                return Err(func.unwrap_err());
+            }
+
+            Ok(func.unwrap()());
+        }
     }
 }
