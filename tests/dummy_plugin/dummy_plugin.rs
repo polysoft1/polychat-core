0a6722caad3f88c67ec54ab0f98fe9e675faa6b7
jaredoconnell <jared.oc321@gmail.com>
2022-01-30 22:58:02 -0500

    Updated to support plugin message-communication branch

--- tests/dummy_plugin/dummy_plugin.rs
+++ tests/dummy_plugin/dummy_plugin.rs
@@ -6,6 +6,7 @@ use std::boxed::Box;
 
 use polychat_plugin::types::Account;
 use polychat_plugin::plugin::PluginInfo;
+use polychat_plugin::plugin::Message;
 
 extern "C" fn create_account() -> Account {
     Box::into_raw(Box::new(5)) as Account
@@ -15,6 +16,12 @@ extern "C" fn print(acc: Account) {
     let data = acc as *mut u8;
     unsafe {
         println!("Hello {}", *data);
+    }
+}
+
+extern "C" fn post_message(msg: * const Message) {
+    unsafe {
+        println!("Instructed to post message with body {}", *(*msg).body);
     }
 }
 
@@ -28,5 +35,6 @@ extern "C" fn destroy_account(acc: Account) {
 unsafe extern "C" fn initialize(info: *mut PluginInfo) {
     (*info).create_account = Some(create_account);
     (*info).destroy_account = Some(destroy_account);
+    (*info).post_message = Some(post_message);
     (*info).print = Some(print);
 }
